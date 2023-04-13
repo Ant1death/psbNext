1c9956a94da0685f40834295e2ef54abac8e6d39
IVKrylova <ivk.lis@yandex.ru>
2023-04-13 15:22:28 +0300

    fix: fix links in the footer

--- compontens/AppFooter/AppFooter.js
+++ compontens/AppFooter/AppFooter.js
@@ -1,18 +1,20 @@
+import Link from 'next/link';
 import style from '../../styles/AppFooter.module.scss'
 
 function appFooter() {
-    return (
+  return (
     <footer className={style.footer}>
-        <p className={style.footer__copyright}>© PSB Hosting. All Rights Reserved</p>
-        <div className={style.footer__wrapper}>
-          <nav>
-            <a href="#">О компании</a>
-            <a href="#">О компании</a>
-            <a href="#">О компании</a>
-            <a href="#">О компании</a>
-          </nav>
-        </div>
+      <p className={style.footer__copyright}>© PSB Hosting. All Rights Reserved</p>
+      <div className={style.footer__wrapper}>
+        <nav>
+          <Link href="/company" className={style.navbar__link}>О компании</Link>
+          <Link href="/vps" className={style.navbar__link}>VPS</Link>
+          <Link href="/abuse" className={style.navbar__link}>Bulletproof VPS</Link>
+          <Link href="/vpn" className={style.navbar__link}>VPN</Link>
+        </nav>
+      </div>
     </footer>
-    )
+  );
 }
+
 export default appFooter;
