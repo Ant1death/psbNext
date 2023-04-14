691efe671400c91bd08513d6306f8fda52916dee
IVKrylova <ivk.lis@yandex.ru>
2023-04-14 19:05:34 +0300

    feat: add a page login

---
+++ compontens/AuthForm/AuthForm.js
@@ -0,0 +1,35 @@
+import Link from 'next/link';
+import { faArrowRightLong } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import style from '../../styles/Auth.module.scss';
+
+const AuthForm = ({ title, children, button, bottomLink, bottomLinkHref }) => {
+  return (
+    <form className={style['form']}>
+      <p className={style['form__title']}>
+        {title}
+      </p>
+      {children}
+      <button type='submit' className={style['form__button-submit']}>
+        {button}
+      </button>
+      {button === 'Вход' &&
+        <p className={style['form__link-reset-password']}>
+          Забыли&nbsp;
+          <Link href='/accounts/password/reset/' className={style['form__link']}>
+            имя пользователя / пароль?
+          </Link>
+        </p>
+      }
+      <Link href={bottomLinkHref} className={`${style['form__link']} ${style['form__link-bottom']}`}>
+        {bottomLink}
+        <FontAwesomeIcon
+          icon={faArrowRightLong}
+          className={style['form__bottom-link-icon']}
+        />
+      </Link>
+    </form>
+  );
+}
+
+export default AuthForm;
--- pages/accounts/login.js
+++ pages/accounts/login.js
@@ -1,9 +1,51 @@
-//import style from '../../styles/Ath.module.scss';
+import AuthForm from '../../compontens/AuthForm/AuthForm';
+import { faEnvelope, faLock } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import style from '../../styles/Auth.module.scss';
 
 export default function Login() {
   return (
-    <>
-      <div className=""></div>
-    </>
+    <main className={style['container']}>
+      <section className={style['content']}>
+        <img className={style['content__logo']} alt='logo' src='/logo.png' />
+        <AuthForm
+          title='Авторизация'
+          button='Вход'
+          bottomLink='Создать аккаунт'
+          bottomLinkHref='/accounts/signup/'
+        >
+          <label className={style['input']} htmlFor='name'>
+            <input
+              type='text'
+              name='name'
+              id='name'
+              required
+              className={style['input__field']}
+              placeholder='Имя пользователя'
+            />
+            <span className={style['input__field-focus']}></span>
+            <FontAwesomeIcon
+              icon={faEnvelope}
+              className={style['input__field-icon']}
+            />
+          </label>
+          <label className={style['input']} htmlFor='password'>
+            <input
+              type='password'
+              name='password'
+              id='password'
+              required
+              className={style['input__field']}
+              placeholder='Пароль'
+            />
+            <span className={style['input__field-focus']}></span>
+            <FontAwesomeIcon
+              icon={faLock}
+              className={style['input__field-icon']}
+             />
+          </label>
+        </AuthForm>
+      </section>
+    </main>
   );
 }
