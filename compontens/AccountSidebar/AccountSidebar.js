c55c6f82b677d8872fe66b45bfa109fbd739326e
IVKrylova <ivk.lis@yandex.ru>
2023-04-16 15:35:41 +0300

    feat: add a header of the sidebar

--- compontens/AccountSidebar/AccountSidebar.js
+++ compontens/AccountSidebar/AccountSidebar.js
@@ -1,9 +1,27 @@
+import Link from 'next/link';
 import style from '../../styles/AccountSidebar.module.scss';
 
 const AccountSidebar = () => {
   return (
     <aside className={style['sidebar']}>
+      <div className={style['sidebar__header']}>
+        <Link href='/account'>
+          <img src='/logo.png' alt='logo' />
+        </Link>
+      </div>
+      <nav className={style['sidebar__nav-menu']}>
+        <ul className={style['sidebar__nav-menu-list']}>
+          <li>
+            <h3 className={style['sidebar__subtitle']}>
+              Главная страница
+            </h3>
+            <Link href='/account'>
 
+              Панель управления
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </aside>
   );
 }
