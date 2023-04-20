350061e222d67c66959588cbff50d06e5974712f
IVKrylova <ivk.lis@yandex.ru>
2023-04-20 11:42:23 +0300

    feat: add a switcing cards

--- pages/account/shop/index.js
+++ pages/account/shop/index.js
@@ -25,6 +25,12 @@ export default function AccountVps() {
 
   const classButtonTable = `${style['shop__display-button']} ${isTableActive ? style['shop__display-button_active'] : ''}`;
   const classButtonList = `${style['shop__display-button']} ${isListActive ? style['shop__display-button_active'] : ''}`;
+  const classItem = `${style['card']} ${style['shop__item']} ${isListActive ? style['shop__item_list'] : ''}`;
+  const imgItemClass = `${style['shop__item-img']} ${isListActive ? style['shop__item-img_list'] : ''}`;
+  const classWrapTitle = `${style['shop__item-wrap-title']} ${isListActive ? style['shop__item-wrap-title_list'] : ''}`;
+  const classItemTitle = `${style['shop__item-title']} ${isListActive ? style['shop__item-title_list'] : ''}`;
+  const classPriceWrapItem = `${style['shop__item-price-wrap']} ${isListActive ? style['shop__item-price-wrap_list'] : ''}`;
+  const classPriceItem = `${style['shop__item-price']} ${isListActive ? style['shop__item-price_list'] : ''}`;
 
   const handleCountryClick = (evt) => {
     const country = evt.target.textContent;
@@ -43,6 +49,11 @@ export default function AccountVps() {
 
   const handleSearchItem = (evt) => {
     setSeachedItem(evt.target.value);
+  }
+
+  const toggleLookCards = () => {
+    isTableActive ? setIsTableActive(false) : setIsTableActive(true);
+    isListActive ? setIsListActive(false) : setIsListActive(true);
   }
 
   useEffect(() => {
@@ -152,12 +163,20 @@ export default function AccountVps() {
           </form>
           <ul className={style['shop__display']}>
             <li>
-              <button type='button' className={classButtonTable}>
+              <button
+                type='button'
+                className={classButtonTable}
+                onClick={toggleLookCards}
+              >
                 <iconify-icon icon="fa-solid:th"></iconify-icon>
               </button>
             </li>
             <li>
-              <button type='button' className={classButtonList}>
+              <button
+                type='button'
+                className={classButtonList}
+                onClick={toggleLookCards}
+              >
                 <iconify-icon icon="fa:list"></iconify-icon>
               </button>
             </li>
@@ -166,28 +185,35 @@ export default function AccountVps() {
         <ul className={style['shop__card-list']}>
           {currentCountry.map(el => {
             return (
-              <li key={el.id} className={`${style['card']} ${style['shop__item']}`}>
-                <h2 className={style['shop__item-title']}>
-                  <Link href={`/account/shop/new/[id]`}>
-                    {`${el.title} - ${el.country}`}
-                  </Link>
-                </h2>
-                <ul className={style['shop__item-list']}>
-                  <li>{el.DMCA}</li>
-                  <li>{el.vCPU}</li>
-                  <li>{el.RAM}</li>
-                  <li>{el.SSD}</li>
-                  <li>{el.KVM}</li>
-                  <li>{el.Gbps}</li>
-                  <li>{el.bandwidth}</li>
-                </ul>
-                <p className={style['shop__item-price']}>
-                  {el.price}
-                </p>
-                <Link href={`/account/shop/new/[id]`} className={style['shop__button-cta']}>
-                  <iconify-icon icon="ci:shopping-cart-02"></iconify-icon>
-                  &nbsp;Мгновенная покупка
+              <li key={el.id} className={classItem}>
+                <Link href={`/account/shop/new/[id]`} className={imgItemClass}>
+                  <img src='/server.png' alt='icon server' />
                 </Link>
+                <div className={classWrapTitle}>
+                  <h2 className={classItemTitle}>
+                    <Link href={`/account/shop/new/[id]`}>
+                      {`${el.title} - ${el.country}`}
+                    </Link>
+                  </h2>
+                  <ul className={style['shop__item-list']}>
+                    <li>{el.DMCA}</li>
+                    <li>{el.vCPU}</li>
+                    <li>{el.RAM}</li>
+                    <li>{el.SSD}</li>
+                    <li>{el.KVM}</li>
+                    <li>{el.Gbps}</li>
+                    <li>{el.bandwidth}</li>
+                  </ul>
+                </div>
+                <div className={classPriceWrapItem}>
+                  <p className={classPriceItem}>
+                    {isListActive ? el.price.split('/')[0] : el.price}
+                  </p>
+                  <Link href={`/account/shop/new/[id]`} className={style['shop__button-cta']}>
+                    <iconify-icon icon="ci:shopping-cart-02"></iconify-icon>
+                    &nbsp;Мгновенная покупка
+                  </Link>
+                </div>
               </li>
             );
           })}
