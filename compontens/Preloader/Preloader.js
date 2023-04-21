73c08cbec65278d292521766fbb8579802d8814a
IVKrylova <ivk.lis@yandex.ru>
2023-04-21 10:52:00 +0300

    feat: add a preloader

---
+++ compontens/Preloader/Preloader.js
@@ -0,0 +1,13 @@
+import style from '../../styles/Preloader.module.scss';
+
+const Preloader = ({ isLoading }) => {
+  return (
+    <img
+      alt='preloader'
+      className={`${style.preloader} ${isLoading ? style['preloader_visible'] : ''}`}
+      src='/loader.svg'
+    />
+  );
+}
+
+export default Preloader;
