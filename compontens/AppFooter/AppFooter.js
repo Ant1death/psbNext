db4b9d91e3f5e79153fc32b1e16d2882a11acbbb
IVKrylova <ivk.lis@yandex.ru>
2023-04-29 21:14:16 +0300

    feat: add reviews

--- compontens/AppFooter/AppFooter.js
+++ compontens/AppFooter/AppFooter.js
@@ -5,14 +5,60 @@ function appFooter() {
   return (
     <footer className={style.footer}>
       <p className={style.footer__copyright}>© PSB Hosting. All Rights Reserved</p>
-      <div className={style.footer__wrapper}>
-        <nav>
-          <Link href="/company" className={style.navbar__link}>О компании</Link>
-          <Link href="/vps" className={style.navbar__link}>VPS</Link>
-          <Link href="/abuse" className={style.navbar__link}>Bulletproof VPS</Link>
-          <Link href="/vpn" className={style.navbar__link}>VPN</Link>
-        </nav>
-      </div>
+      <nav className={style.footer__wrapper}>
+        <ul>
+          <li>
+            <Link href='/company' className={style.navbar__link}>О компании</Link>
+          </li>
+          <li>
+            <Link href='/vps' className={style.navbar__link}>VPS</Link>
+          </li>
+          <li>
+            <Link href='/abuse' className={style.navbar__link}>Bulletproof VPS</Link>
+          </li>
+          <li>
+            <Link href='/vpn' className={style.navbar__link}>VPN</Link>
+          </li>
+        </ul>
+      </nav>
+      <ul className={style.footer__reviews}>
+        <li>
+          <Link
+            href='https://ru.hostings.info/psb-hosting-pro.html'
+            target='_blank'
+            title='Отзывы о хостинге psb hosting'
+          >
+            <img alt='Отзывы клиентов на Hostings.info' src='/hostinginfo.png' />
+          </Link>
+        </li>
+        <li>
+          <Link
+            href='https://hosting101.ru/psb-hosting'
+            target='_blank'
+            title='Отзывы о хостинге psb hosting'
+          >
+            <img alt='Отзывы клиентов на Hosting101.ru' src='/hosting101.png' />
+          </Link>
+        </li>
+        <li>
+          <Link
+            href='https://hostinghub.ru/psb-hosting'
+            target='_blank'
+            title='Отзывы о хостинге psb hosting'
+          >
+            <img alt='Отзывы клиентов на HostingHub.ru' src='/hostinghub.png' />
+          </Link>
+        </li>
+        <li>
+          <Link
+            href='https://vpsup.ru/reviews/psb-hosting.pro.html'
+            target='_blank'
+            title='Отзывы о хостинге psb hosting'
+          >
+            <img alt='Отзывы клиентов на vpsUp.ru' src='/vpsup.png' />
+          </Link>
+        </li>
+      </ul>
     </footer>
   );
 }
