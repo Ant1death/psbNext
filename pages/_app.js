718aeed5840099ea11cc7a28e00dc97e42567677
IVKrylova <ivk.lis@yandex.ru>
2023-05-16 15:33:18 +0300

    feat: set up a store

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
+import { Provider } from 'react-redux';
+
+import { wrapper } from '../store/store';
+
 import '../styles/globals.css';
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, ...pageProps }) {
+  const { store, props } = wrapper.useWrappedStore(pageProps);
   const getLayout = Component.getLayout || ((page) => page);
 
-  return getLayout(<Component {...pageProps} />);
+  return (
+    <Provider store={store}>
+      {getLayout(<Component {...props} />)}
+    </Provider>
+  );
+
+
 }
