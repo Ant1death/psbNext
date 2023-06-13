0c0526498b248de10a50abf4ec42274b71bb76af
IVKrylova <ivk.lis@yandex.ru>
2023-06-13 12:56:38 +0300

    fix: fix a loading of the lending

--- compontens/Layout/Layout.js
+++ compontens/Layout/Layout.js
@@ -1,14 +1,41 @@
 import { Rubik } from '@next/font/google';
+import Head from 'next/head';
+import { useState, useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
+
 import AppHeader from '../AppHeader/AppHeader';
 import AppFooter from '../AppFooter/AppFooter';
 import ButtonTelegram from '../ButtonTelegram/ButtonTelegram';
-import Head from 'next/head';
+import Preloader from '../Preloader/Preloader';
 
 const rubik = Rubik({
   subsets: ['latin', 'cyrillic'],
 });
 
 const Layout = ({ children, title }) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const { ready } = useTranslation('translation');
+
+  const handleLoad = () => {
+    if (isLoading) setTimeout(() => setIsLoading(false), 1000);
+  }
+
+  useEffect(() => {
+    if (ready) {
+      // for Chrome
+      window.addEventListener('load', handleLoad);
+
+      // for yandex browser
+      if (isLoading && document.readyState === 'complete') {
+        handleLoad();
+      }
+
+      return () => {
+        window.removeEventListener('load', handleLoad);
+      };
+    }
+  }, [ready]);
+
   return (
     <>
       <Head>
@@ -18,8 +45,9 @@ const Layout = ({ children, title }) => {
         <title>{`PSB Hosting ${title ? title : ''}`}</title>
         <link rel="shortcut icon" type="image/x-icon" href="/images/favicon.ico" />
       </Head>
+      {isLoading && <Preloader /> }
       <div className={rubik.className}>
-        <div className="container">
+        <div className={`container ${isLoading ? 'hidden' : ''}`}>
           <AppHeader />
             { children }
           <AppFooter />
