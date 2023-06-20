d73852d6c7f144b27689653391f2e5e3588fa802
IVKrylova <ivk.lis@yandex.ru>
2023-06-20 21:22:05 +0300

    fix: fix img

--- pages/index.js
+++ pages/index.js
@@ -42,8 +42,8 @@ export default function Home() {
           <Image
             src="/main_img.png"
             alt="PSB hosting image"
-            width={522}
-            height={472}
+            width='522'
+            height='472'
             className={style.img}
           />
         </div>
