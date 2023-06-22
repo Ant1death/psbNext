6fe98c035df0e33b56430e4ac363592a05a29ab5
IVKrylova <ivk.lis@yandex.ru>
2023-06-22 21:27:33 +0300

    fix: fix sort

--- utils/sortByCountries.js
+++ utils/sortByCountries.js
@@ -1,13 +1,3 @@
-/* const countries = ['Nederland', 'Moldova', 'Hong Kong', 'USA', 'canada']; const products = [{ c: 'Hong Kong' }, { c: 'USA' }, { c: 'Nederland' }, { c: 'Hong Kong' }, { c: 'Nederland' }, { c: 'Africa' }];
-products.sort((a, b) => {
-  const indexA = countries.indexOf(a.c);
-  const indexB = countries.indexOf(b.c);
-if (indexA === -1 && indexB === -1) { return 0; }
-else if (indexA === -1) { return 1; }
-else if (indexB === -1) { return -1; }
-else { return indexA - indexB; } });
-console.log(products); // [{c:'Nederland'},{c:'Nederland'},{c:'Hong Kong'},{c:'Hong Kong'},{c:'USA'},{c:'Africa'}]
- */
 const countries = [
   'Netherlands',
   'Moldova',
